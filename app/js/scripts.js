8f1ca5b4a6155948418213863103f5381a3c3268
Wesley Chitwood <wesley.chitwood1@gmail.com>
2016-06-09 00:04:36 -0400

    adding mobile navigation, finishing navigation for responsive layouts with hamburger menu and font awesome with transitions and beginning to set up isotope masonry

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,41 +1,7 @@
-
-
-jQuery(function($) {
-
-  $('body').scrollspy({
-    target: '#stickyheader',
-    offset: 40
-  });
-
-  $(window).scroll(function() {
-  
-    var scrollVar = $(window).scrollTop();
-  
-    if (scrollVar > 770) {
-      $('#stickyheader').addClass('sticky');
-      
-    }
-  
-    if (scrollVar < 770) {
-      $('#stickyheader').removeClass('sticky');
-     
-    }
-  });
-
- 
-
-  $('#stickyheader ul li a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-
-    var target = this.hash;
-    var $target = $(target);
-
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top - 77
-    }, 980, 'swing', function() {
-      window.location.hash = target;
-    });
-  });
-
-
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer'
+  }
 });
